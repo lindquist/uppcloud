46a1ef289963858f72f2da82c09bed35c2fe2b41
Tomas Lindquist <tomas.l.olsen@gmail.com>
2023-01-25 18:55:34 +0100

    Initial commit RSA with RS256 support

---
+++ RSA/RSA.h
@@ -0,0 +1,52 @@
+#ifndef _RSA_RSA_h
+#define _RSA_RSA_h
+
+#include <Core/SSL/SSL.h>
+
+namespace UppCloud {
+	
+using namespace ::Upp;
+
+struct RSAPrivateKey : Moveable<RSAPrivateKey>
+{
+	RSA* priv = nullptr;
+	
+	RSAPrivateKey()			{}
+	RSAPrivateKey(const String& pem_string)
+							{ SetKey(pem_string); }
+	~RSAPrivateKey();
+	
+	operator RSA*() const	{ return priv; }
+	operator bool() const	{ return priv != nullptr; }
+	
+	bool SetKey(const String& pem_string);
+	String GetKey() const;
+	bool Generate(int bits, BN_ULONG e = RSA_F4);
+	RSA* GetPublicKey() const;
+
+	String SignRS256(const String& text) const;
+};
+
+struct RSAPublicKey : Moveable<RSAPublicKey>
+{
+	RSA *pub = nullptr;
+	
+	RSAPublicKey()		{}
+	RSAPublicKey(const String& pem_string)
+							{ SetKey(pem_string); }
+	RSAPublicKey(RSA* pubkey)
+							{ pub = pubkey; }
+	~RSAPublicKey();
+	
+	operator RSA*() const	{ return pub; }
+	operator bool() const	{ return pub != nullptr; }
+	
+	bool SetKey(const String& pem);
+	String GetKey() const;
+	
+	bool VerifyRS256(const String& text, const String& signature) const;
+};
+
+} // UppCloud
+
+#endif
